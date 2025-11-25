73560b4536bed2d76f05b700c5d5acb3a8eb05ee
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2025-11-25 23:45:12 +0300

    Project image arrays are separated into JS file

---
+++ ustundagyapi/src/data/projectsdata.js
@@ -0,0 +1,30 @@
+import image01 from "../assets/1.jpg";
+import image02 from "../assets/2.jpg";
+import image03 from "../assets/3.jpg";
+import image04 from "../assets/4.jpg";
+import image05 from "../assets/5.jpg";
+import image06 from "../assets/6.jpg";
+import image07 from "../assets/7.jpg";
+import image08 from "../assets/8.jpg";
+import image09 from "../assets/9.jpg";
+import image10 from "../assets/10.jpg";
+import image11 from "../assets/11.jpg";
+import image12 from "../assets/12.jpg";
+
+export const existingProjects = [
+  { id: 1, title: "Kalem Tekstil", img: image01 },
+  { id: 2, title: "Reymo A.Ş.", img: image02 },
+  { id: 3, title: "Finansbank", img: image03 },
+  { id: 4, title: "Beşikler Deri", img: image04 },
+  { id: 5, title: "Volvo Grup", img: image08 },
+  { id: 6, title: "Akkaya A.Ş. Tekstil", img: image07 },
+  { id: 7, title: "", img: image05 },
+  { id: 8, title: "", img: image06 },
+  { id: 9, title: "", img: image09 },
+  { id: 10, title: "", img: image10 },
+];
+
+export const continuedProjects = [
+  { id: 11, title: "", img: image11 },
+  { id: 12, title: "", img: image12 },
+];
