a0820d2231cf2bb732b3fee4b0e1ea2601437378
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2025-11-27 19:25:06 +0300

    Content component added to About page

--- ustundagyapi/src/pages/About.jsx
+++ ustundagyapi/src/pages/About.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
-import HeaderContent from "../components/HeaderContent";
 import Footer from "../components/Footer";
+import AboutContent from "../components/AboutContent";
 
 export default function About() {
   return (
     <div className="about">
       <Header />
+      <AboutContent />
       <Footer />
     </div>
   );
