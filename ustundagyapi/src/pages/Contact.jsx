d0f0af867835cf157c10bb4665fdc84031618341
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2025-12-01 23:04:57 +0300

    Contact page built

--- ustundagyapi/src/pages/Contact.jsx
+++ ustundagyapi/src/pages/Contact.jsx
@@ -1,11 +1,30 @@
 import Header from "../components/Header";
-import HeaderContent from "../components/HeaderContent";
 import Footer from "../components/Footer";
+import ContactForm from "../components/ContactForm";
+import Line from "../components/Line";
+import "../styles/contact.css";
+import ContactInfo from "../components/ContactInfo";
 
 export default function Contact() {
   return (
     <div className="contact">
       <Header />
+
+      <div className="contact-wrapper">
+        <div className="contact-title">
+          <Line />
+          <h2>Birlikte Daha Sağlam Yarınlara</h2>
+          <div>
+            Her adımda kalite, her projede güven. Sorularınız ve proje
+            talepleriniz için uzman ekibimizle hemen iletişime geçin.
+          </div>
+
+          <ContactInfo />
+        </div>
+
+        <ContactForm />
+      </div>
+
       <Footer />
     </div>
   );
